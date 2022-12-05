c901bd9bece97d5d6ad5a59859e018d8396bab17
Hugo Pauthier <hugo.pauthier@gmail.com>
2022-12-05 12:22:02 +0000

    Day 4

---
+++ days/4/part2.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+var _ = require('lodash');
+
+function main(sectionList) {
+  let res = 0;
+  for (const [elf1, elf2] of sectionList) {
+    const range1 = _.range(elf1[0], elf1[1] + 1);
+    const range2 = _.range(elf2[0], elf2[1] + 1);
+    if (_.intersection(range1, range2).length > 0) {
+        res++;
+    }
+  }
+  return res;
+}
+
+const sectionList = fs
+  .readFileSync('./input.txt', 'utf8')
+  .split('\n')
+  .map((pairs) => pairs.split(',').map((section) => section.split('-').map(Number)));
+
+console.log(main(sectionList));
